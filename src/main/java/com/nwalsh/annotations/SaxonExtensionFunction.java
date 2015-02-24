5150b611b473d9a91251ef2ba90f9828370423d2
Norman Walsh <ndw@nwalsh.com>
2015-02-24 16:31:00 -0600

    JavaDoc and other tweaks for Maven deployment

--- src/main/java/com/nwalsh/annotations/SaxonExtensionFunction.java
+++ src/main/java/com/nwalsh/annotations/SaxonExtensionFunction.java
@@ -8,10 +8,24 @@ import java.lang.annotation.Target;
 import org.atteo.classindex.IndexAnnotated;
 
 /**
- * Created by ndw on 2/19/15.
+ * An annotation for identifying classes that implement SaxonExtensionFunctions.
+ * <p>
+ * XML Calabash will automatically attempt to register any extension functions that so annotated.
+ *
+ * @author ndw
+ * @version 1.0
  */
 @Retention(RetentionPolicy.RUNTIME)
 @IndexAnnotated
 public @interface SaxonExtensionFunction {
-    // This annotation identifies a class that implements a Saxon 9.x extension function
+    /**
+     * A logging level hint.
+     *
+     * The warning level is a hint to the runtime processor about how agressively to report
+     * instantiation (and other errors). For completely standalone classes, it's reasonable
+     * to give quite assertive warnings (e.g. INFO). For classes that rely on third party
+     * libraries that may or may not be present, a less assertive warning (e.g. DEBUG)
+     * might be more reasonable.
+     */
+    String warnLevel() default "INFO";
 }
---
+++ src/main/java/com/nwalsh/annotations/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * "Global" annotations for use in multiple projects.
+ * <P>
+ * These classes are annotations used, for example, by XML Calabash to find Saxon extension functions.
+ * The annotations are shared across multiple projects that might otherwise have no reason to be
+ * dependent on each other. Putting them in a separate package means they don't have to be.
+ *
+ * @author ndw
+ * @version 1.0
+ */
+
+package com.nwalsh.annotations;
